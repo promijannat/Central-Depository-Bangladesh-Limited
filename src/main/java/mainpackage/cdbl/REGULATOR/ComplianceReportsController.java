b15fcd9ac7f3afa55790e85c163fd94c5773b774
M.H. Rafi <m.h.rafi436@gmail.com>
2025-08-03 01:07:43 +0600

    Added some direcory and packages

---
+++ src/main/java/mainpackage/cdbl/REGULATOR/ComplianceReportsController.java
@@ -0,0 +1,12 @@
+package mainpackage.cdbl.REGULATOR;
+
+import javafx.scene.control.TextArea;
+
+public class ComplianceReportsController
+{
+    @javafx.fxml.FXML
+    private TextArea Remarks_txt;
+
+    @javafx.fxml.FXML
+    public void initialize() {
+    }}
